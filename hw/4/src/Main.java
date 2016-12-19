040fddba57b60a6acef6ff359df2c893147e27b6
Martin Melka <melkamar@fit.cvut.cz>
2016-12-19 19:00:57 +0100

    Delete useless Main

--- hw/4/src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Martin Melka on 05.12.2016 0:45.
- */
-public class Main {
-    public static void main(String [] args){
-        System.out.println("Hello world yeah.");
-    }
-}
